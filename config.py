537d753eed2abc9f2f10b21b1ac891f1d0438f5f
Chloe <dev@lexian.dev>
2025-05-18 16:46:06 -0400

    fix: update app version to 0.7.1 in configuration

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Configuration
 APP_NAME = "GoExport"
-APP_VERSION = "0.6.1"
+APP_VERSION = "0.7.1"
 DEFAULT_DEPENDENCIES_FILENAME = "dependencies"
 DEFAULT_LIBS_FILENAME = "libs"
 DEFAULT_OUTPUT_FILENAME = "data"
