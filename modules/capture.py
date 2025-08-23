b9a1ff54b099910ef4971b72d978fcac877c6e2b
Chloe <dev@lexian.dev>
2025-08-23 15:02:43 -0400

    Refactor retrieve method to consolidate data retrieval from OBS and native capture

--- modules/capture.py
+++ modules/capture.py
@@ -10,7 +10,7 @@ class Capture:
         self.startup_delay = None
         self.ended_delay = None
         self.process = None
-
+        self.filename = None
         self.obs = ObsCapture()
         self.native = NativeCapture()
         self.is_obs = False
@@ -22,16 +22,30 @@ class Capture:
         except Exception as e:
             logger.error(f"Failed to connect to OBS WebSocket server: {e}")
 
+    def retrieve(self):
+        if self.is_obs:
+            self.filename = self.obs.filename
+            self.start_time = self.obs.start_time
+            self.end_time = self.obs.end_time
+            self.startup_delay = self.obs.startup_delay
+            self.ended_delay = self.obs.ended_delay
+        else:
+            self.filename = self.native.filename
+            self.start_time = self.native.start_time
+            self.end_time = self.native.end_time
+            self.startup_delay = self.native.startup_delay
+            self.ended_delay = self.native.ended_delay
+
     def start(self, output: str, width: int, height: int):
         if self.is_obs:
             object = self.obs.start(output, width, height)
         else:
             object = self.native.start(output, width, height)
-        
-        # Load the values reported by the recorder
+        self.retrieve()
         return object
     
     def stop(self):
+        self.retrieve()
         if self.is_obs:
             return self.obs.stop()
         else:
