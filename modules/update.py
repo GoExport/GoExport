5e0414f92d0ba3ee88e6fb9e31e9d8b3a0fe34cd
Chloe <dev@lexian.dev>
2025-10-17 23:46:37 -0400

    Add update checking functionality to manage application versioning

---
+++ modules/update.py
@@ -0,0 +1,44 @@
+import requests
+import helpers
+from modules.logger import logger
+
+class Update:
+    def __init__(self):
+        repo = helpers.get_config("APP_REPO")
+        self.repo_api = f"https://api.github.com/repos/{repo}/releases/latest"
+        self.version_key = "latest_version"
+
+    def _check_for_update(self):
+        try:
+            response = requests.get(self.repo_api, timeout=10)
+            response.raise_for_status()
+            data = response.json()
+
+            latest_version = data.get("tag_name", "").lstrip("v")
+            local_version = helpers.get_config("APP_VERSION").lstrip("v")
+
+            if local_version != latest_version:
+                helpers.save("update_needed", latest_version)
+                return latest_version
+            else:
+                return False
+        except Exception as e:
+            logger.error(f"Error when checking for updates: {e}")
+
+    def _should_check_for_updates(self):
+        current_time = helpers.get_timestamp("Update check")
+        adjusted = current_time + 3_600_000  # +1 hour
+        last_checked = helpers.load("updates_checked", 0)
+
+        if current_time < last_checked:
+            self.current_update = helpers.load("update_needed")
+            return False
+
+        helpers.save("updates_checked", adjusted)
+        return True
+
+    def check(self):
+        if self._should_check_for_updates():
+            return self._check_for_update()
+        else:
+            return self.current_update
